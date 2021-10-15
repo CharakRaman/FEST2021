bd446cc85f18dbf4cae1261c46756f9b96311ab1
Vishvesh-Codehunt <patelvishvesh85@gmail.com>
2021-10-15 07:53:11 +0530

    done linear search

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,10 +1,10 @@
-def linearSearch(arr, findKey):
-    for value in arr:
-        if findKey == value:
-            return f'{findKey} Key is Found at index : {arr.index(value) + 1}'
-    return "Key not found"
-
-
+def linearSearch(array, key):
+    count = 0
+    for num in array:
+        if num==key:
+            return (f"The {key} number is found at {count} position")
+        count = count+1
+    return (f"The {key} is not in the array")
 
 print(linearSearch([1,2,3,4,5,6], 3))
 print(linearSearch([1,2,3,4,5,6], 9))
